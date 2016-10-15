1a76ef3a3439938d7137b1082345779ece70bde3
Lisa Saalfrank <lsaalfrank@hof-university.de>
2016-10-16 01:25:08 +0200

    Add translations
    Adds translations for views and validators

---
+++ src/View/Translation.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Equidea\View;
+
+/**
+ * @author      Lisa Saalfrank <lisa.saalfrank@web.de>
+ * @copyright   2016 Lisa Saalfrank
+ * @license     MIT License http://opensource.org/licenses/MIT
+ * @package     Equidea\View
+ */
+class Translation {
+    
+    /**
+     * @var string
+     */
+    private $path;
+    
+    /**
+     * @var string
+     */
+    private $extension;
+    
+    /**
+     * @var string
+     */
+    private $language;
+    
+    /**
+     * @var string
+     */
+    private $name;
+    
+    /**
+     * @var array
+     */
+    private $translations = [];
+    
+    /**
+     * @param   string  $path
+     * @param   string  $lang
+     * @param   string  $name
+     * @param   string  $extension
+     */
+    public static __construct(
+        string $path,
+        string $lang,
+        string $name,
+        string $extension
+    ) {
+        $this->path = $path;
+        $this->extension = $extension;
+        $this->language = $language;
+        $this->extension = $extension;
+        $this->loadTranslations();
+    }
+    
+    /**
+     * @return  void
+     */
+    private function loadTranslations()
+    {
+        // Build the path to the translation file
+        $path = $this->path.$this->lang.'/'.$this->name.$this->extension;
+        
+        // If the file exists, load its content.
+        if (file_exists($path)) {
+            $this->translations = include $path;
+        }
+    }
+    
+    /**
+     * @param   string  $name
+     *
+     * @return  string
+     */
+    public function translate(string $name):string
+    {
+        // If a translation is available, return its value.
+        if (isset($this->translations[$name])) {
+            return $this->translations[$name];
+        }
+        // Otherwise return the translation name.
+        return $name;
+    }
+}
--- src/View/View.php
+++ src/View/View.php
@@ -13,20 +13,32 @@ class View {
     /**
      * @var string
      */
-    public $path = '../app/views/';
+    private $viewPath = '../app/views/';
     
     /**
      * @var string
      */
-    public $extension = '.php';
+    private $langPath = '../app/lang/';
+    
+    /**
+     * @var string
+     */
+    private $extension = '.php';
+    
+    /**
+     * @var string
+     */
+    private $lang = 'EN';
     
     /**
      * @param   array   $config
      */
     public function __construct(array $config)
     {
-        $this->path = $config['path'];
+        $this->viewPath = $config['viewPath'];
+        $this->langPath = $config['langPath'];
         $this->extension = $config['extension'];
+        $this->lang = $config['lang'];
     }
     
     /**
@@ -37,7 +49,16 @@ class View {
      */
     public function render(string $name, array $data = []):string
     {
-        $template = new Template($this->path, $name, $this->extension, $data);
+        $template = new Template($this->viewPath, $name, $this->extension, $data);
         return $template->render();
+    }
+    
+    /**
+     * @param   string  $name
+     *
+     * @return  \Equidea\View\Translation
+     */
+    public function translation($name) {
+        return new Translation($this->langPath, $this->lang, $name, $this->extension);
     }
 }
