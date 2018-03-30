16a95db70bedf0ce05c90094d6bee7d4044df3f9
Lisa Saalfrank <lsaalfrank@hof-university.de>
2018-03-30 20:15:18 +0200

    Add GenCalculators

---
+++ src/Engine/Genetics/AbstractGenCalculator.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Equidea\Engine\Genetics;
+
+/**
+ * @author      Lisa Saalfrank <lisa.saalfrank@web.de>
+ * @copyright   2016-2018 Lisa Saalfrank
+ * @license     MIT License http://opensource.org/licenses/MIT
+ * @package     Equidea\Engine\Genetics
+ */
+abstract class AbstractGenCalculator {
+
+    /**
+     * @var array
+     */
+    protected $alleleNames = [];
+
+    /**
+     * @param   array   $alleleNames
+     */
+    public function __construct(array $alleleNames) {
+        $this->alleleNames = $alleleNames;
+    }
+
+    /**
+     * @param   array   $fGenes
+     * @param   array   $mGenes
+     *
+     * @return  array
+     */
+    protected function calculateAllAlleles(array $fGenes, array $mGenes) : array
+    {
+        $cGenes = [];
+        foreach ($this->alleleNames as $allele) {
+            $cGenes[$allele] = $this->calculateSingleAllele($fGenes, $mGenes);
+        }
+        return $cGenes;
+    }
+
+    /**
+     * @param   int $father
+     * @param   int $mother
+     *
+     * @return  int
+     */
+    protected function calculateSingleAllele(int $father, int $mother) : int
+    {
+        $fGene = $this->getRandomGene($father);
+        $mGene = $this->getRandomGene($mother);
+        return ($fGene < $mGene) ? $mGene . $fGene : $fGene . $mGene;
+    }
+
+    /**
+     * @param   int $allele
+     *
+     * @return  int
+     */
+    protected function getRandomGene(int $allele) : int
+    {
+        $genes = str_split($allele);
+        $random = mt_rand(0,1);
+        return (int)$genes[$random];
+    }
+}
---
+++ src/Engine/Genetics/ColorGenCalculator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Equidea\Engine\Genetics;
+
+use Equidea\Engine\Entity\ColorGenEntity;
+
+/**
+ * @author      Lisa Saalfrank <lisa.saalfrank@web.de>
+ * @copyright   2016-2018 Lisa Saalfrank
+ * @license     MIT License http://opensource.org/licenses/MIT
+ * @package     Equidea\Engine\Genetics
+ */
+ class ColorGenCalculator extends AbstractGenCalculator {
+
+    public function __construct() {
+        parent::__construct(ColorGenEntity::$alleleNames);
+    }
+
+    /**
+     * @param   \Equidea\Engine\Entity\ColorGenEntity   $father
+     * @param   \Equidea\Engine\Entity\ColorGenEntity   $mother
+     *
+     * @return  \Equidea\Engine\Entity\ColorGenEntity
+     */
+    public function calculateFoal(
+        ColorGenEntity $father,
+        ColorGenEntity $mother
+    ) : ColorGenEntity
+    {
+        $fGenes = $father->getAllAlleles();
+        $mGenes = $mother->getAllAlleles();
+        $cGenes = $this->calculateAllAlleles($fGenes, $mGenes);
+
+        $child = new ColorGenEntity();
+        $child->setAllAlleles($cGenes);
+        return $child;
+    }
+}
